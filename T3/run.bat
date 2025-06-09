929c95c3e4e6c322745298dca6b0fcae89032729
W <jonathan.wnog@gmail.com>
2025-06-08 22:49:33 -0300

    changing terminal

--- T3/run.bat
+++ T3/run.bat
@@ -1,6 +1,6 @@
 @echo off
 del /s /q *.class
 javac *.java
-start "Client 5000" cmd /k "title Client 5000 && java Client localhost 5000"
-start "Client 5001" cmd /k "title Client 5001 && java Client localhost 5001"
-start "Client 5002" cmd /k "title Client 5002 && java Client localhost 5002"
+start powershell.exe -NoExit -Command "& { $Host.UI.RawUI.WindowTitle = 'Client 5000'; java Client 127.0.0.1 5000 }"
+start powershell.exe -NoExit -Command "& { $Host.UI.RawUI.WindowTitle = 'Client 5001'; java Client 127.0.0.1 5001 }"
+start powershell.exe -NoExit -Command "& { $Host.UI.RawUI.WindowTitle = 'Client 5002'; java Client 127.0.0.1 5002 }"
